4c6376e9c36f4f5aa9bea7476e50f959b4b47e43
Alex Gurrola <alex.gurrola@versidyne.com>
2015-03-15 02:13:14 -0700

    Added Output Types

--- src/Vexis/CoreBundle/Controller/InitController.php
+++ src/Vexis/CoreBundle/Controller/InitController.php
@@ -2,27 +2,60 @@
 
 namespace Vexis\CoreBundle\Controller;
 
+// Symfony Framework
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+// HTTP
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+// Serializer
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
+
+/**
+ * Class InitController
+ * @package Vexis\CoreBundle\Controller
+ */
 class InitController extends Controller
 {
     /**
      * @Route("/{furl}")
      * @Template()
-     * @param $furl
+     * @param Request $request
+     * @param null $furl
      * @return array|Response
      */
-    public function indexAction($furl = null)
+    public function indexAction(Request $request, $furl = null)
     {
-        if ($furl == 'render') {
-            return $this->render('VexisCoreBundle:Init:blog.html.twig', array('furl' => $furl));
-        } elseif ($furl == 'response') {
-            return new Response("<html><body>Raw Response: {$furl}</body></html>");
-        } else {
-            return array('furl' => $furl);
+        //POST: $request->request->get('');
+
+        $outputData = array('furl' => $furl);
+        $outputType = strtolower($request->query->get('output'));
+
+        if ($outputType) {
+            $normalizer = new GetSetMethodNormalizer();
+            if ($outputType == 'json') {
+                $encoder = new JsonEncoder();
+            } elseif ($outputType == 'xml') {
+                $encoder = new XmlEncoder();
+            } else {
+                $encoder = false;
+            }
+            if ($encoder != false) {
+                $serializer = new Serializer(array($normalizer), array($encoder));
+                $serializedContent = $serializer->serialize(@$outputData ?: null, $outputType);
+            } else {
+                $serializedContent = "Output Type: {$outputType} is not supported.";
+            }
+            return new Response($serializedContent);
         }
+
+        return $this->render('VexisCoreBundle:Init:blog.html.twig', $outputData);
+
     }
 }
