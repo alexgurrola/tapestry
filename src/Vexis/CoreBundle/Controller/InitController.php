e6c413057194d71a1811408cd4e11b9f71f1db13
Alex Gurrola <alex.gurrola@versidyne.com>
2015-03-15 06:23:21 +0000

    Refactored for Expansion

--- src/Vexis/CoreBundle/Controller/InitController.php
+++ src/Vexis/CoreBundle/Controller/InitController.php
@@ -7,18 +7,18 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Response;
 
-class DefaultController extends Controller
+class InitController extends Controller
 {
     /**
-     * @Route("/{furl}", furl="")
+     * @Route("/{furl}")
      * @Template()
      * @param $furl
      * @return array|Response
      */
-    public function indexAction($furl)
+    public function indexAction($furl = null)
     {
         if ($furl == 'render') {
-            return $this->render('default/index.html.twig', array('furl' => $furl));
+            return $this->render('VexisCoreBundle:Init:blog.html.twig', array('furl' => $furl));
         } elseif ($furl == 'response') {
             return new Response("<html><body>Raw Response: {$furl}</body></html>");
         } else {
